e89bd52d0dc7461698ea2c00634f4552fe6f55eb
Arag0rn <63494926+Arag0rn@users.noreply.github.com>
2024-01-24 23:35:10 +0100

    started signUp page

--- components/SignUpComp.tsx
+++ components/SignUpComp.tsx
@@ -1,15 +1,42 @@
+"use client"
 import React from 'react'
 import styles from "./SignUp.module.scss"; 
+import { useState } from 'react';
+import Container from './Container';
 
 
 const SignUpComp= () => {
+
+  const [activeButton, setActiveButton] = useState<string>('SIGN UP');
+
+  const handleButtonClick = (buttonName: string) => {
+    setActiveButton(buttonName);
+  };
+
+
   return (
     <>
-    <h1 className={styles.head}>
-        Hello I am SignUp page
-    </h1>
+    <Container>
+  <div className={styles.btnBox}>
+      <button
+          className={` ${activeButton === 'SIGN UP' ? styles.smallButton1 : styles.secondaryButton}`}
+          onClick={() => handleButtonClick('SIGN UP')}
+        >
+          SIGN UP
+        </button>
+        <button
+          className={` ${activeButton === 'SIGN IN' ? styles.smallButton2 : styles.secondaryButton}`}
+          onClick={() => handleButtonClick('SIGN IN')}
+        >
+          SIGN IN
+      </button>
+
+  </div>
+
+  <h2>Create new account</h2>
 
     <button className={styles.signupBtn} type='submit'>SIGN UP</button>
+    </Container>
     </>
   )
 }
