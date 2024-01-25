8053b55d380e2157e2ca982096db19edd405c10e
Arag0rn <63494926+Arag0rn@users.noreply.github.com>
2024-01-26 00:46:21 +0100

    fixed bg-image, addet info page

---
+++ src/app/components/Information/Information.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import TogleBtn from '../SignUp/TogleBtn'
+import Container from '../Container'
+
+
+const Information = () => {
+  return (
+    <Container>
+    <TogleBtn/>
+    <h2>We need to know about you a little bit more</h2>
+    </Container>
+  )
+}
+
+export default Information
--- src/app/components/VerifyEmail/VerifyEmail.tsx
+++ src/app/components/VerifyEmail/VerifyEmail.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import styles from '../SignUp/SignUp.module.scss';
+import styles from './verify.module.scss';
 import Container from '../Container';
 
 const VerifyEmail = () => {
   return (
     <Container>
     <h2 className={styles.head}>Verify your email</h2>
+    <p className={styles.subHead}>Please check up your email and confirm the message to complete the registration</p>
+
+    <button className={styles.resentBtn}>RESENT EMAIL</button>
+    <div className={styles.bottomLine}></div>
     </Container>
   )
 }
