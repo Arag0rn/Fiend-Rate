8b64c04a015026ca3f9525ad949b5afbb23450d7
Arag0rn <63494926+Arag0rn@users.noreply.github.com>
2024-01-26 13:15:28 +0100

    addded format.date, choise of gender

--- src/app/components/Information/Information.tsx
+++ src/app/components/Information/Information.tsx
@@ -1,5 +1,6 @@
 "use client"
 import React from 'react';
+import { useState } from 'react';
 import TogleBtn from '../SignUp/TogleBtn';
 import Container from '../Container';
 import { Formik, Field, Form, ErrorMessage } from 'formik';
@@ -7,15 +8,43 @@ import styles from "./information.module.scss";
 import male from "../../images/Information/Male.svg";
 import female from "../../images/Information/Female.svg";
 import other from "../../images/Information/Other.svg";
+import malePressed from "../../images/Information/MalePressed.svg";
+import femalePressed from "../../images/Information/FemalePressed.svg";
+import otherPressed from "../../images/Information/OtherPressed.svg";
 import Image from 'next/image';
+import * as Yup from 'yup';
 
-interface FormValues {
-  username: string;
-  birthday: string;
 
-}
+const createAccountSchema = Yup.object().shape({
+  username: Yup.string()
+    .min(2, 'Too Short!')
+    .max(50, 'Too Long!')
+    .required('Required'),
+    birthday: Yup.string()
+    .required('Please enter your birthdate'),
+});
 
 const Information = () => {
+  const [date, setDate] = useState('');
+  const [gender, setGender] = useState('');
+
+
+  console.log(gender);
+
+
+  const handleInputChange = (event) => {
+    const input = event.target.value.replace(/\D/g, ''); 
+    const formattedDate = input
+      .replace(/^(\d{2})(\d{2})(\d{0,4})/, '$1.$2.$3') 
+      .replace(/^(\d{2}\.\d{2})(\d{4})/, '$1.$2'); 
+    setDate(formattedDate);
+  };
+
+  const handleOptionClick = (option) => {
+    setGender((prevGender) => (prevGender === option ? '' : option));
+  };
+
+
   return (
     <Container>
       <TogleBtn />
@@ -25,7 +54,9 @@ const Information = () => {
         initialValues={{
           username: '',
           birthday: '',
+          gender: '',
         }}
+        validationSchema={createAccountSchema}
         onSubmit={async (values, action) => {
             
         }}
@@ -36,34 +67,42 @@ const Information = () => {
           <ErrorMessage className={styles.errMes} component="span" name="username" />
 
           <label className={styles.fieldLabel} htmlFor="birthday">Birthday</label>
-          <Field className={styles.field} id="birthday" name="birthday" placeholder="DD.MM.YYYY" />
+          <Field className={styles.field} 
+          type="text"
+          id="birthday" 
+          name="birthday" 
+          value={date}
+          onChange={handleInputChange}
+          maxLength="10"
+          placeholder="DD.MM.YYYY" />
           <ErrorMessage className={styles.errMes} component="span" name="birthday" />
 
           <ul className={styles.genderBox}>
-        <p className={styles.boxTxt}>Gender</p>
-         <li className={styles.genderItem}>
-          <Image
-            src={male}
-            alt="male"
-          />
-          <p className={styles.genderTxt}>male</p>
-        </li>
+      <p className={styles.boxTxt}>Gender</p>
+      <li
+        className={styles.genderItem} 
+        onClick={() => handleOptionClick('male')}
+      >
+        <Image src={gender === "male"? malePressed : male} alt="male" />
+        <p className={styles.genderTxt}>male</p>
+      </li>
 
-        <li className={styles.genderItem}>
-          <Image
-            src={female}
-            alt="female"
-          />
-          <p className={styles.genderTxt}>female</p>
-        </li>
-        <li className={styles.genderItem}>
-          <Image
-            src={other}
-            alt="other"
-          />
-          <p className={styles.genderTxt}>other</p>
-        </li>
-      </ul>
+      <li
+        className={styles.genderItem} 
+        onClick={() => handleOptionClick('female')}
+      >
+        <Image src={gender === "female"? femalePressed : female} alt="female" />
+        <p className={styles.genderTxt}>female</p>
+      </li>
+
+      <li
+        className={styles.genderItem} 
+        onClick={() => handleOptionClick('other')}
+      >
+        <Image src={gender === "other"? otherPressed : other} alt="other" />
+        <p className={styles.genderTxt}>other</p>
+      </li>
+    </ul>
 
           
           <button className={styles.continueBtn} type="submit" disabled={true}>CREATE ACCOUNT</button>
