7f029182b48c214faff378bea071f022c14080ba
Arag0rn <63494926+Arag0rn@users.noreply.github.com>
2024-02-07 21:54:33 +0100

    Fixed styles, added validation, disabled btns

--- src/app/components/SignUp/TogleBtn.tsx
+++ src/app/components/SignUp/TogleBtn.tsx
@@ -1,34 +1,57 @@
 "use client"
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useState } from 'react';
 import styles from "./SignUp.module.scss"; 
 import Link from 'next/link';
 
+
 const TogleBtn = () => {
+
     const [activeButton, setActiveButton] = useState<string>('SIGN UP');
+    const [enabledTogleBtn, setEnabledTogleBtn]= useState<boolean>(true);
 
     const handleButtonClick = (buttonName: string) => {
         setActiveButton(buttonName);
       };
     
 
-  return (
-    <div className={styles.btnBox}>
-    <Link href="/signup"
-        className={` ${activeButton === 'SIGN UP' ? styles.smallButton1 : styles.secondaryButton}`}
-        onClick={() => handleButtonClick('SIGN UP')}
-      >
-        SIGN UP
-      </Link>
-      <Link href="/sign-in"
-        className={` ${activeButton === 'SIGN IN' ? styles.smallButton2 : styles.secondaryButton2}`}
-        onClick={() => handleButtonClick('SIGN IN')}
-      >
-        SIGN IN
-    </Link>
+      useEffect(() => {
+        if ( window.location.pathname !== '/signup' && window.location.pathname !== '/sign-in') {
+          setEnabledTogleBtn(false)
+        }
+      }, []);
 
-    </div>
-  )
+      return (
+        <div className={styles.btnBox}>
+          {enabledTogleBtn ? (
+            <>
+              <Link href="/signup"
+                  className={`${activeButton === 'SIGN UP' ? styles.smallButton1 : styles.secondaryButton}`}
+                  onClick={() => handleButtonClick('SIGN UP')}
+                  >
+                  SIGN UP
+          
+              </Link>
+              <Link href="/sign-in"
+                  className={`${activeButton === 'SIGN IN' ? styles.smallButton2 : styles.secondaryButton2}`}
+                  onClick={() => handleButtonClick('SIGN IN')}
+                >
+                  SIGN IN
+  
+              </Link>
+            </>
+          ) : (
+            <>
+              <div className={styles.secondaryButton}>
+                SIGN UP
+              </div>
+              <div className={styles.secondaryButton2}>
+                SIGN IN
+              </div>
+            </>
+          )}
+        </div>
+      );
 }
 
 export default TogleBtn
