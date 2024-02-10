a2cc6d3b3f4c071c9d9614b49f9d66202e081931
Arag0rn <63494926+Arag0rn@users.noreply.github.com>
2024-02-10 11:57:18 +0100

    changed mt into vh

--- src/app/components/SignUp/SignUpComp.tsx
+++ src/app/components/SignUp/SignUpComp.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React from 'react'
+import React, { useEffect } from 'react'
 import styles from "./SignUp.module.scss"; 
 import { useState } from 'react';
 import Container from '../Container';
@@ -45,9 +45,8 @@ const SignUpComp= () => {
 
   const [showPassword, setShowPassword] = useState(false);
   const [isAccept, setIsAccept] = useState(false);
-
-
-
+  
+ 
   return (
     <>
     <Container>
