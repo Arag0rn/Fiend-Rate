e19942a4311bf313c6940f95a899855028cf9c16
Arag0rn <63494926+Arag0rn@users.noreply.github.com>
2024-05-08 14:23:30 +0200

    Update RomContext.tsx

--- src/app/components/Context/RomContext.tsx
+++ src/app/components/Context/RomContext.tsx
@@ -10,12 +10,12 @@ import Peer from "peerjs";
 import { v4 as uuidV4 } from 'uuid';
 
 
-const server = 'http://localhost:3000'
-// const server2 = 'https://whispering-falls-70384-f5d92e367b77.herokuapp.com'  
+// const server = 'http://localhost:3000'
+const server2 = 'https://whispering-falls-70384-f5d92e367b77.herokuapp.com'  
 
 export const RoomContext = createContext<any | null>(null);
 
-const ws = socketIOClient(server);
+const ws = socketIOClient(server2);
 
 export const RoomProvider = ({children}) => {
     const router = useRouter();
