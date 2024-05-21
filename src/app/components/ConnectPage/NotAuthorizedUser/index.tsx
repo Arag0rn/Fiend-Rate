7850e4153072b9dfdfed3dfc3eec480bec161678
Arag0rn <63494926+Arag0rn@users.noreply.github.com>
2024-05-21 14:24:02 +0200

    Update index.tsx

--- src/app/components/ConnectPage/NotAuthorizedUser/index.tsx
+++ src/app/components/ConnectPage/NotAuthorizedUser/index.tsx
@@ -6,12 +6,12 @@ import microfon from '../../../images/microfon.svg';
 import UserName from '../UserName';
 import Image from 'next/image';
 
-const NotAuthorizedUser = ({ spinner, search, isConnected, lng, call, pathname }) => {
+const NotAuthorizedUser = ({ spinner, search, isConnected, lng, call }) => {
   return (
     <div className={!spinner ? styles.main : styles['main-call']}>
       <div className={styles["main__content"]}>
         <Stroke spinner={spinner}>
-          <Circle lng={lng} pathname={pathname} search={search} isConnected={isConnected} call={call}>
+          <Circle lng={lng} search={search} isConnected={isConnected} call={call}>
             <Image src={microfon} alt='Microfon' className={styles['main__avatar']} />
             <UserName>{!search && 'User 123'}</UserName>
           </Circle>
